2fe66a5c46c2a0bc01f872c29e7680fa4911f25c
dennisligare <dennisligare@gmail.com>
2022-06-19 16:20:31 +0300

    Added other dashboards

---
+++ individual/dashboard.php
@@ -0,0 +1,72 @@
+<?php
+
+session_start();
+
+require '../db.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,300;1,500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../style/index.css">
+  <title>Court management system</title>
+</head>
+
+<body>
+
+  <header id="header">
+    <div class="display">
+      <div class="logo-1">
+        <img src="../img/CourtOfArms.png" alt="">
+        <p>The Judiciary</p>
+      </div>
+      <div class="logo-2">
+        <img src="../img/logo1.png" alt="">
+        <span>Utumishi kwa wote.</span>
+      </div>
+    </div>
+
+    <nav class="menu">
+      <ul>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="../logout.php">Logout</a></li>
+      </ul>
+
+      <div class="burger-container">
+        <button><i class="fa fa-bars mobile-menu" aria-hidden="true"></i></button>
+        <button><i class="fa fa-times-circle close-menu" aria-hidden="true"></i></button>
+      </div>
+    </nav>
+
+
+  </header>
+
+  <div id="main-dashboard">
+    <div class="container">
+      <div class="welcome">
+        <p>Welcome, <?php echo $_SESSION['username'] ?></p>
+      </div>
+
+    </div>
+
+  </div>
+
+  <footer>
+    <p>WBCCMS &copy; 2022, All Rights Reserved</p>
+  </footer>
+
+  <script type="text/javascript" src="./js/mobilemenu.js"></script>
+</body>
+
+</html>
